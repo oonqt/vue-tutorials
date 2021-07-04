8732846a9a9dff9597601321e1ae1f76c3f21b3a
oonqt <r0blox1053@outlook.com>
2021-07-03 17:59:46 -0700

    What the hell

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -2,14 +2,30 @@ const app = Vue.createApp({
     // template: '<h2>VUE Template</h2>'
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
-        changeTitle(_title) {
-            this.title = _title;
+        // changeTitle(_title) {
+        //     this.title = _title;
+        // },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data) {
+            console.log('Handled', e.type, e);
+            if (data) {
+                console.log(data);
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 });
